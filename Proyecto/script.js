2755328a732537f38367117bd5ec027ed3e62e52
JoseManuelNunez <99033210+JoseManuelNunez@users.noreply.github.com>
2022-03-02 16:12:09 -0400

    Add files via upload

---
+++ Proyecto/script.js
@@ -0,0 +1,21 @@
+document.getElementById("nombre").value;
+console.log(nombre);
+
+//guardar_locals()
+
+
+
+/*
+function guardar_locals(){
+    let carro ={
+        color: "rojo",
+        marca: "tesla",
+        puertas: 4
+    };
+
+    let carro1 ="carrito"
+
+
+    localStorage.setItem("nombre", document.getElementById("nombre".value));
+}
+*/
